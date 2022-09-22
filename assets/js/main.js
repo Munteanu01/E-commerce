d9139cc8768c6d092cdc6984c757ff0e81d1742e
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-09-22 14:25:16 +0300

    Updated scroll effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 ScrollReveal().reveal('.plantersImg',{ delay: 1000 ,origin:'right', distance: '200px'});
 ScrollReveal().reveal('.plantsImg',{ delay: 1000 ,origin:'left', distance: '200px'});
-ScrollReveal().reveal('.plantsLink',{ delay: 1000});
-ScrollReveal().reveal('.plantersLink',{ delay: 1000});
+ScrollReveal().reveal('.plantsLink',{ delay: 1500});
+ScrollReveal().reveal('.plantersLink',{ delay: 1500});
 
