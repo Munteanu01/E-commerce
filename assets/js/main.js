fd3672972dadc56c09b53f108d7824e549a1b9ec
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-09-22 14:22:39 +0300

    Adding scroll effects

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.plantersImg',{ delay: 1000 ,origin:'right', distance: '200px'});
+ScrollReveal().reveal('.plantsImg',{ delay: 1000 ,origin:'left', distance: '200px'});
+ScrollReveal().reveal('.plantsLink',{ delay: 1000});
+ScrollReveal().reveal('.plantersLink',{ delay: 1000});
+
