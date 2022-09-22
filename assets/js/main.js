39cb0b939028420ef696c5dbd8c10d359aaa4018
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-09-22 14:40:19 +0300

    Redesign the scroll effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-ScrollReveal().reveal('.plantersImg',{ delay: 1000 ,origin:'right', distance: '200px'});
-ScrollReveal().reveal('.plantsImg',{ delay: 1000 ,origin:'left', distance: '200px'});
-ScrollReveal().reveal('.plantsLink',{ delay: 1500});
-ScrollReveal().reveal('.plantersLink',{ delay: 1500});
+ScrollReveal().reveal('.greenSpan',{ delay: 1000});
+ScrollReveal().reveal('.plantsText',{ delay: 500 ,origin:'right', distance: '200px'});
+ScrollReveal().reveal('.plantersText',{ delay: 500 ,origin:'left', distance: '200px'});
+ScrollReveal().reveal('.plantsLink',{ delay: 2000});
+ScrollReveal().reveal('.plantersLink',{ delay: 2000});
 
