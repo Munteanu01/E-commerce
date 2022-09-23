466a5351e9d7edcac620fe8cc2b980ce4ab0a686
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-09-23 14:23:13 +0300

    Before disaster

--- assets/js/shopping.js
+++ assets/js/shopping.js
@@ -1,7 +1,11 @@
 const filter = document.querySelector('#filter');
+const filterPlus = document.querySelector('.filterPlus');
+const filterMinus = document.querySelector('.filterMinus');
 const filterDiv = document.querySelectorAll('.filterDiv');
 const item = document.querySelectorAll('.item');
 filter.addEventListener('click', () => {
+    filterPlus.classList.toggle('d-none')
+    filterMinus.classList.toggle('d-none')
     filterDiv.forEach(filterDiv => {
         filterDiv.classList.toggle('d-none');
     })
