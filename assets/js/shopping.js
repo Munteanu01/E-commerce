2b9b9da01fe75d77a68a6e6d111f7615a7a6b79e
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-09-24 11:23:26 +0300

    adjustements

--- assets/js/shopping.js
+++ assets/js/shopping.js
@@ -10,10 +10,6 @@ filter.addEventListener('click', () => {
     filterDiv.forEach(filterDiv => {
         filterDiv.classList.toggle('d-none');
     })
-    item.forEach(item => {
-        item.classList.toggle('col-lg-3');
-        item.classList.toggle('col-lg-4');
-    })
 })
 const availabilityBtn = document.querySelector('.availabilityBtn');
 const availabilityDiv = document.querySelector('.availabilityDiv');
