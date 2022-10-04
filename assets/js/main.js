3e19e7d25b78bc2e49300e2f354b4b438cd7310a
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-04 19:57:56 +0300

    q

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-ScrollReveal().reveal('.greenSpan',{ delay: 1000});
-ScrollReveal().reveal('.plantsText',{ delay: 500 ,origin:'right', distance: '200px'});
-ScrollReveal().reveal('.plantersText',{ delay: 500 ,origin:'left', distance: '200px'});
-ScrollReveal().reveal('.plantsLink',{ delay: 2000});
-ScrollReveal().reveal('.plantersLink',{ delay: 2000});
-
-
-
 //FILTER ON CLICK
 const filter = document.querySelector('#filter');
 const filterPlus = document.querySelector('.filterPlus');
---
+++ assets/js/scroll.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.greenSpan',{ delay: 1000});
+ScrollReveal().reveal('.plantsText',{ delay: 500 ,origin:'right', distance: '200px'});
+ScrollReveal().reveal('.plantersText',{ delay: 500 ,origin:'left', distance: '200px'});
+ScrollReveal().reveal('.plantsLink',{ delay: 2000});
+ScrollReveal().reveal('.plantersLink',{ delay: 2000});
+
+
