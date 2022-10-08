b14d6ae36af0ce3daa00c342bdf62f71d36d5d6d
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-08 11:06:46 +0300

    deleting unnecesary

--- assets/js/plants.js
+++ assets/js/plants.js
@@ -62,46 +62,6 @@ document.querySelectorAll('.filters').forEach(filter => {
     })
 })
 
-//FILTERS
-let items = document.querySelectorAll('.item')
-
-document.querySelector('.filterInStock').addEventListener('click', () =>{items.forEach(item => {
-    if(item.textContent.includes('SOLD OUT')){item.classList.toggle('instockdnone')}
-    if(item.classList.contains('outstockdnone')){item.classList.toggle('outstockdnone');document.querySelector('.filterOutOfStock').classList.remove('filtersOnClick')}})})
-document.querySelector('.filterOutOfStock').addEventListener('click', () =>{items.forEach(item => {
-    if(item.textContent.includes('+')){item.classList.toggle('outstockdnone')}
-    if(item.classList.contains('instockdnone')){item.classList.toggle('instockdnone');document.querySelector('.filterInStock').classList.remove('filtersOnClick')}})})
-document.querySelector('.filterBonsai').addEventListener('click', () =>{items.forEach(item => {
-    if(!item.classList.contains('bonsai')){item.classList.toggle('bonsaidnone')}
-    if(item.classList.contains('treednone')){item.classList.toggle('treednone');document.querySelector('.filterTree').classList.remove('filtersOnClick')}})})
-document.querySelector('.filterTree').addEventListener('click', () =>{items.forEach(item => {
-    if(!item.classList.contains('tree')){item.classList.toggle('treednone')}
-    if(item.classList.contains('bonsaidnone')){item.classList.toggle('bonsaidnone');document.querySelector('.filterBonsai').classList.remove('filtersOnClick')}})})
-
-
-
-document.querySelector('.filterBeginner').addEventListener('click', () =>{items.forEach(item => {
-    if(!item.classList.contains('beginner')){item.classList.toggle('beginnerdnone')}})})
-document.querySelector('.filterJapan').addEventListener('click', () =>{items.forEach(item => {
-    if(!item.classList.contains('japan')){item.classList.toggle('japandnone')}})})
-document.querySelector('.filterHardy').addEventListener('click', () =>{items.forEach(item => {
-    if(!item.classList.contains('hardy')){item.classList.toggle('hardydnone')}})})
-
-document.querySelectorAll('.filters').forEach(filter => {
-filter.addEventListener('click', () => {
-    items.forEach(item => {
-        if(item.classList.contains('beginnerdnone') && item.classList.contains('japandnone')){
-            item.classList.toggle('beginnerdnone');item.classList.toggle('japandnone')
-        }
-        if(item.classList.contains('japandnone') && item.classList.contains('hardydnone')){
-            item.classList.toggle('japandnone');item.classList.toggle('hardydnone')
-        }
-        if(item.classList.contains('hardydnone') && item.classList.contains('beginnerdnone')){
-            item.classList.toggle('hardydnone');item.classList.toggle('beginnerdnone')
-        }
-    })
-})
-})
 
 //SEARCH
 document.querySelector('.search').addEventListener("input", e => {
