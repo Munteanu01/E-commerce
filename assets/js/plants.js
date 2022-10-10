46754fc03d524edeb8708025b1e28e66245b3244
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-10 17:43:39 +0300

    q

--- assets/js/plants.js
+++ assets/js/plants.js
@@ -102,7 +102,6 @@ filterInStock.addEventListener('click', () => {
     
 })
 
-
 filterOutStock.addEventListener('click', () => {
     filteredArr = [];
     if(filterOutStock.checked && !filterInStock.checked){
@@ -141,6 +140,86 @@ filterOutStock.addEventListener('click', () => {
     }
 })
 
+
+
+
+
+filterBonsai.addEventListener('click', () => {
+    filteredArr = [];
+    if(filterBonsai.checked && !filterTree.checked){
+        plantsBonsai.forEach(plant => {
+            let productHtml = `<div class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                    <img class="img-fluid" src="${plant.image}" alt="">
+                                    <h5>${plant.name}</h5>
+                                    <p>${plant.price}$</p>
+                                    <button class="btn pb-1 plus">+</button><p class="counter d-inline-block mt-1 mx-4 d-none"></p><button class="btn pb-1 minus d-none">-</button>
+                                </div>`;
+            filteredArr.push(productHtml)})
+        filteredDiv.innerHTML = filteredArr.join('');
+        filteredDiv.classList.remove('d-none')
+        productsDiv.classList.add('d-none')
+    }
+    if(filterBonsai.checked && filterTree.checked){
+        filteredDiv.classList.add('d-none')
+        productsDiv.classList.remove('d-none')
+    }
+    if(!filterBonsai.checked){
+        filteredDiv.classList.add('d-none')
+        productsDiv.classList.remove('d-none')
+    }
+    if(!filterBonsai.checked && filterTree.checked){
+        plantsTree.forEach(plant => {
+            let productHtml = `<div class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                    <img class="img-fluid" src="${plant.image}" alt="">
+                                    <h5>${plant.name}</h5>
+                                    <p>${plant.price}$</p>
+                                    <button class="btn pb-1 plus">+</button><p class="counter d-inline-block mt-1 mx-4 d-none"></p><button class="btn pb-1 minus d-none">-</button>
+                                </div>`;
+            filteredArr.push(productHtml)})
+        filteredDiv.innerHTML = filteredArr.join('');
+        filteredDiv.classList.remove('d-none')
+        productsDiv.classList.add('d-none')
+    }
+    
+})
+
+filterTree.addEventListener('click', () => {
+    filteredArr = [];
+    if(filterTree.checked && !filterBonsai.checked){
+        plantsTree.forEach(plant => {
+            let productHtml = `<div class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                    <img class="img-fluid" src="${plant.image}" alt="">
+                                    <h5>${plant.name}</h5>
+                                    <p>${plant.price}$</p>
+                                    <button class="btn pb-1 plus">+</button><p class="counter d-inline-block mt-1 mx-4 d-none"></p><button class="btn pb-1 minus d-none">-</button>
+                                </div>`;
+            filteredArr.push(productHtml)})
+        filteredDiv.innerHTML = filteredArr.join('');
+        filteredDiv.classList.remove('d-none')
+        productsDiv.classList.add('d-none')
+    }
+    if(filterTree.checked && filterBonsai.checked ){
+        filteredDiv.classList.add('d-none')
+        productsDiv.classList.remove('d-none')
+    }
+    if(!filterTree.checked){
+        filteredDiv.classList.add('d-none')
+        productsDiv.classList.remove('d-none')
+    }
+    if(!filterTree.checked && filterBonsai.checked){
+        plantsBonsai.forEach(plant => {
+            let productHtml = `<div class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                    <img class="img-fluid" src="${plant.image}" alt="">
+                                    <h5>${plant.name}</h5>
+                                    <p>${plant.price}$</p>
+                                    <button class="btn pb-1 plus">+</button><p class="counter d-inline-block mt-1 mx-4 d-none"></p><button class="btn pb-1 minus d-none">-</button>
+                                </div>`;
+            filteredArr.push(productHtml)})
+        filteredDiv.innerHTML = filteredArr.join('');
+        filteredDiv.classList.remove('d-none')
+        productsDiv.classList.add('d-none')
+    }
+})
 
 
 
