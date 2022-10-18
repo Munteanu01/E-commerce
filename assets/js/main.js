d20d3dadf733cf2ac5a6688acd5762c3cb070bc9
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-18 10:06:47 +0300

    q

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,106 @@
 //MAIN
 const plants = [
-    {"id":"1",
+    {"id":"wqeqwq",
     "name":"Juniperus Bonsai",
     "price":45,
     "availability":"in",
     "type":"bonsai",
     "collection":"japan",
     "image":"img/plants/IMG_8775.jpeg"},
-    {"id":"2",
+    {"id":"gwegwr",
     "name":"Phyllanthus",
     "price":20,
     "availability":"in",
     "type":"",
     "collection":"hardy",
     "image":"img/plants/IMG_9244.jpeg"},
-    {"id":"3",
+    {"id":"htees",
     "name":"Chinensis Shimpaku",
     "price":60,
     "availability":"in",
     "type":"bonsai",
     "collection":"japan",
     "image":"img/plants/Kishu_Bonsai_02.jpeg"},
-    {"id":"4",
+    {"id":"saodpas",
     "name":"Monstera Deliciosa",
     "price":25,
     "availability":"in",
     "type":"",
     "collection":"beginner",
     "image":"img/plants/Monstera_ow.jpeg"},
-    {"id":"5",
+    {"id":"vansdg",
     "name":"Olive Tree",
     "price":35,
     "availability":"in",
     "type":"tree",
     "collection":"japan",
     "image":"img/plants/Olive-Tree-L.jpeg"},
-    {"id":"6",
+    {"id":"sarsas",
     "name":"Everfresh Tree",
     "price":50,
     "availability":"in",
     "type":"tree",
     "collection":"japan",
     "image":"img/plants/Everfresh.jpeg"},
-    {"id":"7",
+    {"id":"vdasga",
     "name":"Pachira Aquatica",
     "price":15,
     "availability":"out",
     "type":"",
     "collection":"beginner",
     "image":"img/plants/Pachira_Moon_p.jpeg"},
-    {"id":"8",
+    {"id":"fbdsfs",
     "name":"Variegated Bonsai",
     "price":40,
     "availability":"in",
     "type":"bonsai",
     "collection":"japan",
     "image":"img/plants/Wind-Swept_t.jpeg"},]
+let basketItems = []
+
+
 let plantsDiv = document.querySelector('.plantsDiv')
 let shop = () => {
     return (plantsDiv.innerHTML = plants.map((x)=>{
+        let {id, name, price, image} = x
         if(x.availability === 'in'){
-        return `<div id="product-id-${x.id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
-                                                <img class="img-fluid" src="${x.image}" alt="">
-                                                <h5>${x.name}</h5>
-                                                <p>${x.price}$</p>
-                                                <button class="btn pb-1 plus">+</button><p class="counter d-inline-block mt-1 mx-4 d-none"></p><button class="btn pb-1 minus d-none">-</button>
+        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                                <img class="img-fluid" src="${image}" alt="">
+                                                <h5>${name}</h5>
+                                                <p>${price}$</p>
+                                                <button onclick="increase(${id})" class="btn pb-1 plus">+</button><p id="${id}" class="counter d-inline-block mt-1 mx-4 d-none"></p><button onclick="decrease(${id})"  class="btn pb-1 minus d-none">-</button>
                                             </div>`} 
         if(x.availability === 'out'){
-        return `<div id="product-id-${x.id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
-                                                <img class="img-fluid" src="${x.image}" alt="">
-                                                <h5>${x.name}</h5>
-                                                <p>${x.price}$</p>
+        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                                <img class="img-fluid" src="${image}" alt="">
+                                                <h5>${name}</h5>
+                                                <p>${price}$</p>
                                                 <p class="sold">SOLD OUT</p>
                                             </div>`} 
         
     }).join('') )
 }
 shop()
+let increase = (id) => {
+    let selectedItem = id
+    let searchBasket = basketItems.find((x)=> x.id === selectedItem.id)
+    if(searchBasket === undefined){
+        basketItems.push({
+            id: selectedItem.id,
+            item: 1,
+        })
+        console.log(basketItems)
+    }
+    else{
+        searchBasket.item += 1;
+    }
+    
+}
+let decrease = (id) => {console.log(id)}
+let update = () => {console.log('minus')}
 
 //FILTER
-const items = document.querySelectorAll('.item')
-document.querySelector('#filter').addEventListener('click', () => {
-    document.querySelector('.filterPlus').classList.toggle('d-none');
-    document.querySelector('.filterMinus').classList.toggle('d-none');
-    document.querySelector('.filterDiv').classList.toggle('d-none');})
-document.querySelector('.firstBtn').addEventListener('click', () => {document.querySelector('.firstDiv').classList.toggle('d-none');})
-document.querySelector('.secondBtn').addEventListener('click', () => {document.querySelector('.secondDiv').classList.toggle('d-none');})
-document.querySelector('.thirdBtn').addEventListener('click', () => {document.querySelector('.thirdDiv').classList.toggle('d-none');})
+
 let filteredPlantsDiv = document.querySelector('.filteredPlantsDiv')
 const filters = document.querySelectorAll('input[type="checkbox"')
 const filterInStock = document.querySelector('.filterInStock')
@@ -98,32 +112,43 @@ const filterJapan = document.querySelector('.filterJapan')
 const filterHardy = document.querySelector('.filterHardy')
 const filterOn = () => {
     filteredPlantsDiv.classList.remove('d-none')
-    plantsDiv.classList.add('d-none')}
+    plantsDiv.classList.add('d-none')
+}
 const filterOff = () => {
     filteredPlantsDiv.classList.add('d-none')
-    plantsDiv.classList.remove('d-none')}
+    plantsDiv.classList.remove('d-none')
+}
 filters.forEach(filter => {
 filter.addEventListener('click', () => {
     filter.checked ? filter.nextSibling.classList.add('filtersOnClick') :null
     !filter.checked ? filter.nextSibling.classList.remove('filtersOnClick') :null
     filterInStock.checked || filterOutStock.checked || filterBonsai.checked || filterTree.checked || filterJapan.checked || filterBeginner.checked || filterHardy.checked ? filterOn() :filterOff();
+    
     filteredArr = [];
     for(let plant of plants){
-        if(filterInStock.checked && filterOutStock.checked){plant.availability ==='in' || plant.availability === 'out' ?  null :null}
+        if(filterInStock.checked && filterOutStock.checked){
+            plant.availability ==='in' || plant.availability === 'out' ?  null :null
+        }
         else if(filterInStock.checked &&  plant.availability !== 'in'){continue;}
         else if(filterOutStock.checked && plant.availability !== 'out' ){continue;}
-        if(filterBonsai.checked && filterTree.checked){if(plant.type === 'bonsai' || plant.type === 'tree'){}else{continue;}}
+
+        if(filterBonsai.checked && filterTree.checked){
+            if(plant.type === 'bonsai' || plant.type === 'tree'){}else{continue;}
+        }
         else if(filterBonsai.checked && plant.type !== 'bonsai'){continue;}
         else if(filterTree.checked && plant.type !== 'tree'){continue;}
+
         if(filterBeginner.checked && !filterJapan.checked && !filterHardy.checked && plant.collection !== 'beginner'){continue;}
         if(filterJapan.checked && !filterHardy.checked && !filterBeginner.checked && plant.collection !== 'japan'){continue;}
         if(filterHardy.checked && !filterBeginner.checked && !filterJapan.checked && plant.collection !== 'hardy'){continue;}
+
         if(filterBeginner.checked && filterJapan.checked && !filterHardy.checked){
             if(plant.collection === 'beginner' || plant.collection === 'japan'){}else{continue;}}
         if(filterJapan.checked && filterHardy.checked && !filterBeginner.checked){
             if(plant.collection === 'japan' || plant.collection === 'hardy'){}else{continue;}}
         if(filterHardy.checked && filterBeginner.checked && !filterJapan.checked){
             if(plant.collection === 'hardy' || plant.collection === 'beginner'){}else{continue;}}
+
         plant.availability === 'in' ? plantHtml = `<div class="col-lg-4 col-sm-6 mt-0 text-center item">
                                                 <img class="img-fluid" src="${plant.image}" alt="">
                                                 <h5>${plant.name}</h5>
@@ -141,6 +166,24 @@ filter.addEventListener('click', () => {
     filteredPlantsDiv.innerHTML = filteredArr.join('') 
 })
 })
+
+//FILTER ON CLICK
+const items = document.querySelectorAll('.item')
+document.querySelector('#filter').addEventListener('click', () => {
+    document.querySelector('.filterPlus').classList.toggle('d-none');
+    document.querySelector('.filterMinus').classList.toggle('d-none');
+    document.querySelector('.filterDiv').classList.toggle('d-none');
+})
+document.querySelector('.firstBtn').addEventListener('click', () => {
+    document.querySelector('.firstDiv').classList.toggle('d-none');
+})
+document.querySelector('.secondBtn').addEventListener('click', () => {
+    document.querySelector('.secondDiv').classList.toggle('d-none');
+})
+document.querySelector('.thirdBtn').addEventListener('click', () => {
+    document.querySelector('.thirdDiv').classList.toggle('d-none');
+})
+
 //SEARCH
 document.querySelector('.search').addEventListener("input", e => {
     const value = e.target.value.toLowerCase();
