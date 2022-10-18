694fa6a41c791a903a96c6a362e9e5935d9f880f
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-18 14:43:47 +0300

    q

--- assets/js/plants.js
+++ assets/js/plants.js
@@ -1,7 +1,62 @@
-//MAIN
+//SHOP
+const plants = [
+    {"id":"wqeqwq",
+    "name":"Juniperus Bonsai",
+    "price":45,
+    "availability":"in",
+    "type":"bonsai",
+    "collection":"japan",
+    "image":"img/plants/IMG_8775.jpeg"},
+    {"id":"gwegwr",
+    "name":"Phyllanthus",
+    "price":20,
+    "availability":"in",
+    "type":"",
+    "collection":"hardy",
+    "image":"img/plants/IMG_9244.jpeg"},
+    {"id":"htees",
+    "name":"Chinensis Shimpaku",
+    "price":60,
+    "availability":"in",
+    "type":"bonsai",
+    "collection":"japan",
+    "image":"img/plants/Kishu_Bonsai_02.jpeg"},
+    {"id":"saodpas",
+    "name":"Monstera Deliciosa",
+    "price":25,
+    "availability":"in",
+    "type":"",
+    "collection":"beginner",
+    "image":"img/plants/Monstera_ow.jpeg"},
+    {"id":"vansdg",
+    "name":"Olive Tree",
+    "price":35,
+    "availability":"in",
+    "type":"tree",
+    "collection":"japan",
+    "image":"img/plants/Olive-Tree-L.jpeg"},
+    {"id":"sarsas",
+    "name":"Everfresh Tree",
+    "price":50,
+    "availability":"in",
+    "type":"tree",
+    "collection":"japan",
+    "image":"img/plants/Everfresh.jpeg"},
+    {"id":"vdasga",
+    "name":"Pachira Aquatica",
+    "price":15,
+    "availability":"out",
+    "type":"",
+    "collection":"beginner",
+    "image":"img/plants/Pachira_Moon_p.jpeg"},
+    {"id":"fbdsfs",
+    "name":"Variegated Bonsai",
+    "price":40,
+    "availability":"in",
+    "type":"bonsai",
+    "collection":"japan",
+    "image":"img/plants/Wind-Swept_t.jpeg"},]
 let basketItems = JSON.parse(localStorage.getItem("data")) || []
-
-
 let plantsDiv = document.querySelector('.plantsDiv')
 let shopHtml = () => {
     return (plantsDiv.innerHTML = plants.map((x)=>{
@@ -58,7 +113,8 @@ let update = (id) => {
 }
 let calculator = () => {
     let cartIcon = document.getElementById("cartAmount")
-    cartIcon.innerHTML = basketItems.map((x) => x.item).reduce((x, y)=>x+y, 0)
+    cartIcon.innerHTML = basketItems.map((x) => x.item).reduce((x, y)=>x+y)
+    
 }
 calculator()
 
--- assets/js/secondary.js
+++ assets/js/secondary.js
@@ -1,6 +1,6 @@
 let basketItems = JSON.parse(localStorage.getItem("data")) || []
 let calculator = () => {
     let cartIcon = document.getElementById("cartAmount")
-    cartIcon.innerHTML = basketItems.map((x) => x.item).reduce((x, y)=>x+y, 0)
+    cartIcon.innerHTML = basketItems.map((x) => x.item).reduce((x, y)=>x+y)
 }
 calculator()
