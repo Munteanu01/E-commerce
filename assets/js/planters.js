1968dae019959d4793168a0dad12b1df9f235968
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-21 16:00:19 +0300

    q

--- assets/js/planters.js
+++ assets/js/planters.js
@@ -8,22 +8,84 @@ const filterCream = document.querySelector('.filterCream')
 const filterMedium = document.querySelector('.filterMedium')
 const filterLarge = document.querySelector('.filterLarge')
 const items = document.querySelectorAll('.item')
-const filterOn = () => {
-    filteredPlantersDiv.classList.remove('d-none')
-    plantersDiv.classList.add('d-none')
-}
-const filterOff = () => {
-    filteredPlantersDiv.classList.add('d-none')
-    plantersDiv.classList.remove('d-none')
-}
+document.querySelector('#filter').addEventListener('click', () => {
+    document.querySelector('.filterPlus').classList.toggle('d-none');
+    document.querySelector('.filterMinus').classList.toggle('d-none');
+    document.querySelector('.filterDiv').classList.toggle('d-none');
+})
+document.querySelector('.firstBtn').addEventListener('click', () => {
+    document.querySelector('.firstDiv').classList.toggle('d-none');
+})
+document.querySelector('.secondBtn').addEventListener('click', () => {
+    document.querySelector('.secondDiv').classList.toggle('d-none');
+})
+document.querySelector('.thirdBtn').addEventListener('click', () => {
+    document.querySelector('.thirdDiv').classList.toggle('d-none');
+})
 
-//SHOP
+//SHOP & FILTER
 let basketItems = JSON.parse(localStorage.getItem("data")) || []
 let shopHtml = () => {
-    return (plantersDiv.innerHTML = planters.map((x)=>{
-        let {id, name, price, image} = x
+    filteredArr =[]
+    plantersDiv.innerHTML = planters.map((x)=>{
+        let {id, name, price, image, availability} = x
         let search = basketItems.find((x)=>x.id === id) || [];
-        if(x.availability === 'in'){
+        filters.forEach(filter => {
+            filter.checked ? filter.nextSibling.classList.add('filtersOnClick') :null
+            !filter.checked ? filter.nextSibling.classList.remove('filtersOnClick') :null})
+        let filtering = () =>{
+                    if(filterInStock.checked && filterOutStock.checked){x.availability ==='in' || x.availability === 'out' ?  null :null}
+                    else if(filterInStock.checked &&  x.availability !== 'in')return
+                    else if(filterOutStock.checked && x.availability !== 'out' )return
+            
+                    if(filterCharcoal.checked && filterCream.checked){
+                        if(x.color === 'charcoal' || x.color === 'cream'){}else{return}}
+                    else if(filterCharcoal.checked && x.color !== 'charcoal')return
+                    else if(filterCream.checked && x.color !== 'cream')return
+                    
+                    if(filterMedium.checked && filterLarge.checked){
+                        if(x.size === 'm' || x.size === 'l'){}else{return}}
+                    else if(filterMedium.checked && x.size !== 'm')return
+                    else if(filterLarge.checked && x.size !== 'l')return
+                    !filteredArr.includes(x.id) ? filteredArr.push(x.id) :null
+                }
+                
+            
+            if(filterInStock.checked || filterOutStock.checked || filterCharcoal.checked || filterCream.checked || filterLarge.checked || filterMedium.checked){
+            filtering()
+            return(plantersDiv.innerHTML = filteredArr.map((y) => {
+                if(id === y){
+                if(availability ==="in"){
+                    if(search.item === undefined){
+                        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                                        <img class="img-fluid" src="${image}" alt="">
+                                                        <h5>${name}</h5>
+                                                        <p>${price}$</p>
+                                                        <button onclick="increase(${id})" class="btn plus mb-1">+</button><p id="${id}" class="counter d-inline-block my-0 mx-4 d-none">${0}</p><button onclick="decrease(${id})" class="btn minus mb-1 d-none">-</button>
+                                                    </div>`
+                    }
+                    if(search.item !== undefined){
+                        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                                        <img class="img-fluid" src="${image}" alt="">
+                                                        <h5>${name}</h5>
+                                                        <p>${price}$</p>
+                                                        <button onclick="increase(${id})" class="btn plus mb-1">+</button><p id="${id}" class="counter d-inline-block my-0 mx-4">${search.item}</p><button onclick="decrease(${id})" class="btn minus mb-1">-</button>
+                                                    </div>`
+                    }
+                }
+                if(availability ==="out"){
+                    return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+                                                <img class="img-fluid" src="${image}" alt="">
+                                                <h5>${name}</h5>
+                                                <p>${price}$</p>
+                                                <p class="sold">SOLD OUT</p>
+                                            </div>`
+                }
+            }
+            }).join(''))
+        }
+        else{
+            if(availability === 'in'){
             if(search.item === undefined){
                 return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
                                                 <img class="img-fluid" src="${image}" alt="">
@@ -40,91 +102,20 @@ let shopHtml = () => {
                                                 <button onclick="increase(${id})" class="btn plus mb-1">+</button><p id="${id}" class="counter d-inline-block my-0 mx-4">${search.item === undefined ? 0 : search.item}</p><button onclick="decrease(${id})" class="btn minus mb-1">-</button>
                                             </div>`
             }
-        } 
-        if(x.availability === 'out'){
-        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
+            } 
+            if(availability === 'out'){
+                                        return `<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
                                                 <img class="img-fluid" src="${image}" alt="">
                                                 <h5>${name}</h5>
                                                 <p>${price}$</p>
                                                 <p class="sold">SOLD OUT</p>
-                                            </div>`} 
-    }).join('') )
+                                            </div>`
+            }
+        }
+    }).join('')
+    if(plantersDiv.children.length === 0){plantersDiv.innerHTML = `<h5 class="text-center my-5 py-5">No Results</h5>`}
 }
 shopHtml()
-
-//FILTER
-document.querySelector('#filter').addEventListener('click', () => {
-    document.querySelector('.filterPlus').classList.toggle('d-none');
-    document.querySelector('.filterMinus').classList.toggle('d-none');
-    document.querySelector('.filterDiv').classList.toggle('d-none');
-})
-document.querySelector('.firstBtn').addEventListener('click', () => {
-    document.querySelector('.firstDiv').classList.toggle('d-none');
-})
-document.querySelector('.secondBtn').addEventListener('click', () => {
-    document.querySelector('.secondDiv').classList.toggle('d-none');
-})
-document.querySelector('.thirdBtn').addEventListener('click', () => {
-    document.querySelector('.thirdDiv').classList.toggle('d-none');
-})
-
-let filtering = () =>{
-    filteredArr = [];
-    filters.forEach(filter => {
-    filter.checked ? filter.nextSibling.classList.add('filtersOnClick') :null
-    !filter.checked ? filter.nextSibling.classList.remove('filtersOnClick') :null
-    filterInStock.checked || filterOutStock.checked || filterCharcoal.checked || filterCream.checked || filterLarge.checked || filterMedium.checked ? filterOn() :filterOff();
-    
-    return (planters.map((x) => {
-        if(filterInStock.checked && filterOutStock.checked){x.availability ==='in' || x.availability === 'out' ?  null :null}
-        else if(filterInStock.checked &&  x.availability !== 'in')return
-        else if(filterOutStock.checked && x.availability !== 'out' )return
-
-        if(filterCharcoal.checked && filterCream.checked){
-            if(x.color === 'charcoal' || x.color === 'cream'){}else{return}}
-        else if(filterCharcoal.checked && x.color !== 'charcoal')return
-        else if(filterCream.checked && x.color !== 'cream')return
-        
-        if(filterMedium.checked && filterLarge.checked){
-            if(x.size === 'm' || x.size === 'l'){}else{return}}
-        else if(filterMedium.checked && x.size !== 'm')return
-        else if(filterLarge.checked && x.size !== 'l')return
-       
-        
-        !filteredArr.includes(x.id) ? filteredArr.push(x.id) :null
-        
-    }))
-})
-localStorage.setItem('filteredArr', JSON.stringify(filteredArr));filteringHtml()}
-let filteringHtml = () => {
-filteredArr = JSON.parse(localStorage.getItem('filteredArr'));
-filteredHtml = [];
-filteredPlantersDiv.innerHTML = filteredArr.map((y) => {
-    planters.map((x) => {
-        if(x.id === y){
-            let {id, name, price, image} = x
-            let search = basketItems.find((x)=>x.id === id) || [];
-            if(search.item === undefined){
-                    filteredHtml.push(`<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
-                                                    <img class="img-fluid" src="${image}" alt="">
-                                                    <h5>${name}</h5>
-                                                    <p>${price}$</p>
-                                                    <button onclick="increase(${id})" class="btn plus mb-1">+</button><p id="${id}" class="counter d-inline-block my-0 mx-4 d-none">${search.item === undefined ? 0 : search.item}</p><button onclick="decrease(${id})" class="btn minus mb-1 d-none">-</button>
-                                                </div>`)}
-            if(search.item !== undefined){
-                    filteredHtml.push(`<div id="product-id-${id}" class="col-lg-4 col-sm-6 mt-0 text-center item">
-                                                    <img class="img-fluid" src="${image}" alt="">
-                                                    <h5>${name}</h5>
-                                                    <p>${price}$</p>
-                                                    <button onclick="increase(${id})" class="btn plus mb-1">+</button><p id="${id}" class="counter d-inline-block my-0 mx-4">${search.item === undefined ? 0 : search.item}</p><button onclick="decrease(${id})" class="btn minus mb-1">-</button>
-                                                </div>`) 
-                }
-            
-        }
-    })
-})
-filteredArr.length === 0 ? filteredPlantersDiv.innerHTML = `<h5 class="text-center my-5 py-5">No Results</h5>`: filteredPlantersDiv.innerHTML = filteredHtml.join('')
-}
 
 //NUMBERS
 let increase = (id) => {
@@ -157,7 +148,7 @@ let update = (id) => {
     let searchBasket = basketItems.find((x)=> x.id === id)
     document.getElementById(id).innerHTML = searchBasket.item
     calculator()
-    filteringHtml()
+    shopHtml()
     document.querySelectorAll('.item').forEach(x => {
         if(!x.children[4])return
         if(x.children[4].innerHTML === '0' ){
@@ -176,7 +167,6 @@ let calculator = () => {
     if(basketItems.map((x) => x.item).reduce((x, y)=>x+y, 0) > 0){
     cartIcon.innerHTML = basketItems.map((x) => x.item).reduce((x, y)=>x+y)}
     else{cartIcon.innerHTML = ''}
-    
 }
 calculator()
 
