185eea68d81a0b40e55207d59c0274cfde08a6a6
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-10-22 14:37:22 +0300

    q

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,6 @@
-ScrollReveal().reveal('.greenSpan',{ delay: 1300});
-ScrollReveal().reveal('.plantsText',{ delay: 500 ,origin:'right', distance: '200px'});
-ScrollReveal().reveal('.plantersText',{ delay: 500 ,origin:'left', distance: '200px'});
+ScrollReveal().reveal('.greenSpan',{ delay: 1000});
+ScrollReveal().reveal('.plantsText',{ delay: 300 ,origin:'right', distance: '200px'});
+ScrollReveal().reveal('.plantersText',{ delay: 300 ,origin:'left', distance: '200px'});
 
 
 
